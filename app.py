6f0348af5037f25c9214261cc7ae634303aa7e8b
Mohamed Mohsen <mohsen20501990@gmail.com>
2026-06-02 08:36:13 +0300

    Streamlit app

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+
+st.write(
+    """
+    ## Housing price in Mexico prediction app
+    this app predicts the house's prices in **Mexico**
+    
+    
+    """
+)
+
+st.image("D:\python\codezilla\Projects\housing_in_mexico\mexico.jpg")
+
+st.sidebar.header("User Input Parameters")
+
+def user_input_features():
+    area_m2= st.sidebar.slider("area [m2]", 40.5 , 160.0, 79.5)
+    lat	= st.sidebar.slider("Lat", 19.0 , 25.0, 19.3)
+    lon = st.sidebar.slider("Loan", -105.0, -90.0, -99.0)
+    
+    data={
+        "area_m2": area_m2,
+        "lat": lat,
+        "lon": lon
+    }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader("User Input Parameters")
+st.write(df)
+
+model = joblib.load("D:\python\codezilla\Projects\housing_in_mexico\models\model.pkl")
+
+prediction = model.predict(df)
+st.subheader("Price for this house in USD is: ")
+st.write(prediction)
+
+
+    
